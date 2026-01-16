f97b8e106bb6471dec7dca25e5095f411e8eb5e8
Siddhesh Gite <siddheshgit2004@gmail.com>
2026-01-16 13:00:58 +0530

    Added colors in tailwind.config.js

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-200 font-bold">Edit app/index.tsx to edit this fuck.</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
   );
 }
